026a2e6a7f27669898966a88cf864d93fedec8a9
amanovishnu <geekymano@gmail.com>
2024-11-04 18:10:05 +0530

    info - project 3 added

--- docker/projects/03 redis-counter/index.js
+++ docker/projects/03 redis-counter/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const redis = require("redis");
 const process = require("process");
+const { hostname } = require("os");
 
 const app = express();
 const client = redis.createClient({
-    url: "redis://redis-server:6379",
+    // url: "redis://redis-server:6379",
+    host: "redis-container",
+    port: 6379
 });
 
 client.set("visits", 0); // setting initial value to 0
@@ -12,7 +15,7 @@ client.set("visits", 0); // setting initial value to 0
 app.get('/', (req, res) => {
     // process.exit(0);
     client.get('visits', (err, visits) => {
-        res.send("No of visits is " + visits);
+        res.send("No of visits is " + visits);``
         client.set('visits', parseInt(visits)+1);
     });
 })
